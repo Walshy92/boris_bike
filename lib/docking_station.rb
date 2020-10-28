602968829215e21248805422b658499eeeec46fb
Chris Walsh <highwalshy@gmail.com>
2020-10-28 15:53:45 +0000

    Defined docking method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,20 @@
 class DockingStation
   def release_bike
+    bike = Bike.new
+  end
+  def docking
+    @counter = 0
+    if @counter == 0
+      @counter += 1
+      return 0
+    elsif @counter == 1
+      return "docking station full"
+    end
+  end
+end
 
+class Bike
+  def working?
+    true
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,13 @@
 require_relative "../lib/docking_station.rb"
 
 describe DockingStation do
+  it do
+    expect(subject.release_bike).to be_an_instance_of Bike
+  end
+end
 
+describe Bike do
+  it do
+    expect(subject.working?).to eq (true)
+  end
 end
